1351bcbe27da8b50b488c0ad8267df362a74524d
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-16 09:16:05 +0100

    Add project files.

---
+++ DefaultConstructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultConstructor
+{
+    public class Student
+    {
+        public string Name;
+        public int Age;
+        
+     }    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            bool b = false;
+            Console.WriteLine(x);
+            Console.WriteLine(b);
+            
+        }
+    }
+}
